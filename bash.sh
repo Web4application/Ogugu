bfe53800b6e7c77479f39bbb795100fa967adf22
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-09 02:40:04 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,12 +1,15 @@
-npx degit web4application/project#main my-awesome-project
-cd my-awesome-project
-
 npm install
 npm run dev
-
+npx degit web4application/ogugu#main my-awesome-project
+cd ogugu
 cd embedded-app-sdk-examples/sdk-playground-packages/client
 pnpm link ~/path/to/embedded-app-sdk # this is an example path
 cd embedded-app-sdk-examples/sdk-playground
 pnpm dev
 npm install @discord/embedded-app-sdk
 npm i xyber-ai-chat
+
+git clone https://github.com/f5devcentral/vscode-f5.git
+cd vscode-f5-fast/
+npm install
+code .
