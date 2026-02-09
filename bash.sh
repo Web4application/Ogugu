6ee8cef1dc7251188cd636442ce3d065761e6ec0
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-09 01:52:18 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
-npx degit user/project#main my-project
-cd my-project
+npx degit web4application/project#main my-awesome-project
+cd my-awesome-project
 
 npm install
 npm run dev
@@ -8,3 +8,5 @@ cd embedded-app-sdk-examples/sdk-playground-packages/client
 pnpm link ~/path/to/embedded-app-sdk # this is an example path
 cd embedded-app-sdk-examples/sdk-playground
 pnpm dev
+npm install @discord/embedded-app-sdk
+npm i xyber-ai-chat
