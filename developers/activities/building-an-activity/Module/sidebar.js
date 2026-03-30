24a917dfd605873e112a3b03ac4befa547c892ec
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-30 13:58:05 +0100

    Create sidebar.js

---
+++ developers/activities/building-an-activity/Module/sidebar.js
@@ -0,0 +1,33 @@
+module.exports = {
+  sidebar: [
+    {
+      type: 'category',
+      label: 'Activities',
+      items: [
+        'activities/index',
+        'activities/game-activity',
+        'activities/music-activity',
+        'activities/watch-together',
+        'activities/study-focus',
+        'activities/trivia-activity',
+        'activities/drawing-art',
+        'activities/voice-chat-games',
+        'activities/polls-votes',
+        'activities/coding-dev',
+        'activities/custom-bot-games',
+        'activities/podcast-radio',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Commands',
+      items: [
+        'commands/index',
+        'commands/moderation',
+        'commands/utility',
+        'commands/fun',
+      ],
+    },
+    'README',
+  ],
+};
