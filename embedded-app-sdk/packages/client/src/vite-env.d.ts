3ff0ac36082753ebe11da04f4072db051eabea98
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-30 08:43:44 +0100

    Update vite-env.d.ts

--- embedded-app-sdk/packages/client/src/vite-env.d.ts
+++ embedded-app-sdk/packages/client/src/vite-env.d.ts
@@ -1,1 +1,33 @@
-"decc849d4d7c7e7220f8d5306b2ef36756efde88"
+import {DiscordSDK} from '@discord/embedded-app-sdk';
+const discordSdk = new DiscordSDK(YOUR_OAUTH2_CLIENT_ID);
+
+async function setup() {
+  // Wait for READY payload from the discord client
+  await discordSdk.ready();
+
+  // Pop open the OAuth permission modal and request for access to scopes listed in scope array below
+  const {code} = await discordSdk.commands.authorize({
+    client_id: YOUR_OAUTH2_CLIENT_ID,
+    response_type: 'code',
+    state: '',
+    prompt: 'none',
+    scope: ['identify', 'applications.commands'],
+  });
+
+  // Retrieve an access_token from your application's server
+  const response = await fetch('/.proxy/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const {access_token} = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+}
